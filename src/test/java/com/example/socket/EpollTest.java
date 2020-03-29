3be6f3eebbf9d7eda08de12519a4ff10bed4b0db
luozijian <luozijian@zuzuche.com>
2020-03-29 14:54:09 +0800

    初始化

---
+++ src/test/java/com/example/socket/EpollTest.java
@@ -0,0 +1,15 @@
+package com.example.socket;
+
+/**
+ * @author: luozijian
+ * @date: 2020-03-29 14:51:46
+ * @description:
+ */
+public class EpollTest {
+
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+}
