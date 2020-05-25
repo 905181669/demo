b6b34fe95cf8bbc5f80abfa5039dcac1c685995c
luozijian <luozijian@zuzuche.com>
2020-05-25 21:59:34 +0800

    tomcat war

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,14 +3,24 @@ package com.example.demo;
 import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
 	public static void main(String[] args) {
+
+
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- src/main/java/com/example/demo/rest/BestRest.java
+++ src/main/java/com/example/demo/rest/BestRest.java
@@ -22,6 +22,6 @@ public class BestRest {
 
 
         Enumeration em = request.getHeaderNames();
-        return "BEST";
+        return "测试get";
     }
 }
